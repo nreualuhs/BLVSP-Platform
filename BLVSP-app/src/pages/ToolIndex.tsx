441528492c04855cfa5989f075b9802890db2f7c
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-10-26 03:40:22 -0700

    add header, find a tool button

--- BLVSP-app/src/pages/ToolIndex.tsx
+++ BLVSP-app/src/pages/ToolIndex.tsx
@@ -1,5 +1,6 @@
 import toolsData from '../data/sample_tools.json';
-import { RatingGroup, Table } from "@chakra-ui/react";
+import { Box, Button, Heading, HStack, RatingGroup, Stack, Table, Text } from "@chakra-ui/react";
+import { InfoTip } from '@/components/ui/toggle-tip';
 
 interface Tool {
   id: string;
@@ -15,35 +16,56 @@ function App() {
     
 
     return (
-      <Table.Root size='lg' variant='outline' showColumnBorder>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeader>Name</Table.ColumnHeader>
-            <Table.ColumnHeader>Compatibility</Table.ColumnHeader>
-            <Table.ColumnHeader>Description</Table.ColumnHeader>
-            <Table.ColumnHeader>Overall Ratings</Table.ColumnHeader>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {tools.map((tool) => (
-            <Table.Row key={tool.id}>
-              <Table.Cell>{tool.name}</Table.Cell>
-              <Table.Cell>{tool.compatibility}</Table.Cell>
-              <Table.Cell>{tool.description}</Table.Cell>
-              <Table.Cell>
-                <RatingGroup.Root readOnly count={5} value={tool.rating} size="sm">
-                  <RatingGroup.HiddenInput />
-                  <RatingGroup.Control />
-                </RatingGroup.Root>
+      <Stack gap={4}>
+        <HStack mb={8}>
+          <Heading size="4xl">
+            Tool Index
+          </Heading>
+          <Text fontSize="xs" mt={4}>
+            Last Updated: October 26, 2025 03:26am {/* @todo: shouldn't hardcode this */}
+          </Text>
 
-              </Table.Cell>
+        </HStack>
 
-            </Table.Row>
-          ))}
-        </Table.Body>
-
-      </Table.Root>
+        <HStack gap={-2}>
+          <Button rounded="full"> 
+            Find a Tool
+          </Button>
+          <Box mb={4}>
+            <InfoTip content="hellooo" /> {/* placeholder */}
+          </Box>
+          
+        </HStack>
         
+        <Table.Root size="lg" variant="outline" showColumnBorder>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeader>Name</Table.ColumnHeader>
+              <Table.ColumnHeader>Compatibility</Table.ColumnHeader>
+              <Table.ColumnHeader>Description</Table.ColumnHeader>
+              <Table.ColumnHeader>Overall Ratings</Table.ColumnHeader>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {tools.map((tool) => (
+              <Table.Row key={tool.id}>
+                <Table.Cell>{tool.name}</Table.Cell>
+                <Table.Cell>{tool.compatibility}</Table.Cell>
+                <Table.Cell>{tool.description}</Table.Cell>
+                <Table.Cell>
+                  <RatingGroup.Root readOnly count={5} value={tool.rating} size="sm">
+                    <RatingGroup.HiddenInput />
+                    <RatingGroup.Control />
+                  </RatingGroup.Root>
+
+                </Table.Cell>
+
+              </Table.Row>
+            ))}
+          </Table.Body>
+
+        </Table.Root>
+      </Stack>
     );
 }
 
