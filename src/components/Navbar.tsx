da4413bbc247838c99193a311ea3d884c502b81f
elliotchun <elliotchun2@gmail.com>
2025-10-26 13:08:43 -0700

    Update navbar components to use Chakra UI components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,28 +4,24 @@ import {
   //NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
-  NavigationMenuList,
   //NavigationMenuTrigger,
   //NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
 
-import { Link } from "react-router-dom";
 import { ColorModeButton } from "./ui/color-mode";
 
 function Navbar() {
   return (
     <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuLink><Link to="/">Home</Link></NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink><Link to="createpage">Create Post</Link></NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <ColorModeButton></ColorModeButton>
-        </NavigationMenuItem>
-      </NavigationMenuList>
+      <NavigationMenuItem>
+        <NavigationMenuLink href="/">Home</NavigationMenuLink>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuLink href="createpage">Create Post</NavigationMenuLink>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <ColorModeButton></ColorModeButton>
+      </NavigationMenuItem>
     </NavigationMenu>
 
   );
