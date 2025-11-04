8a18f0ed7e44bd32b5c4c73aaec1b9417872cb70
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-11-04 15:06:16 -0800

    refactor home page items

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,30 @@
 import { Box, Grid, Heading, Link as ChakraLink, Text } from "@chakra-ui/react";
 import { Link as ReactLink } from "react-router";
 
-function App() {
+interface HomePageItemProps {
+  to: string;
+  title: string;
+  description: string;
+};
 
+const HomePageItem = ({ to, title, description }: HomePageItemProps) => {
+  return (
+    <Box>
+      <Heading rounded="sm" bg="primary" px={2} py={4} textAlign="center" mb={2}>
+        <ChakraLink asChild>
+          <ReactLink to={to}>
+            {title}
+          </ReactLink>
+        </ChakraLink>
+      </Heading>
+      <Text rounded="sm" bg="tertiary" px={2} py={4} textAlign="center" height="full">
+        {description}
+      </Text>
+    </Box>
+  );
+};
+
+function App() {
 	return (
 		<>
 			<Heading size="7xl">Access Code Hub</Heading>
@@ -18,36 +40,24 @@ function App() {
 					</Heading>
 					<Text rounded="sm" bg="tertiary" px={2} py={4} textAlign="center" height="full">Our Tool Index page is a configured list of accessible tools. Please visit here to find the most applicable tools for your use case.</Text>
 				</Box>
-				<Box>
-					<Heading rounded="sm" bg="primary" px={2} py={4} textAlign="center" mb={2}>
-						<ChakraLink asChild>
-							<ReactLink to="#">
-								Submit A Tool
-							</ReactLink>
-						</ChakraLink>
-					</Heading>
-					<Text rounded="sm" bg="tertiary" px={2} py={4} textAlign="center" height="full">To submit a tool to the website please use our Tool Submission Form. This will create a new index in the Tool Index page, so that other developers can take advantage of this utility.</Text>
-				</Box>
-				<Box>
-					<Heading rounded="sm" bg="primary" px={2} py={4} textAlign="center" mb={2}>
-						<ChakraLink asChild>
-							<ReactLink to="#">
-								Connect Now
-							</ReactLink>
-						</ChakraLink>
-					</Heading>
-					<Text rounded="sm" bg="tertiary" px={2} py={4} textAlign="center" height="full">To learn more about the BLVSP community please visit our Community section. Here you can find other software professionals to discuss useful tools.</Text>
-				</Box>
-				<Box>
-					<Heading rounded="sm" bg="primary" px={2} py={4} textAlign="center" mb={2}>
-						<ChakraLink>
-							<ReactLink to="#">
-								Get Involved
-							</ReactLink>
-						</ChakraLink>
-					</Heading>
-					<Text rounded="sm" bg="tertiary" px={2} py={4} textAlign="center" height="full">To get more engaged in the community please consider volunteering. Here you can help improve our Tool Index website through content moderation or community outreach.</Text>
-				</Box>
+
+        <HomePageItem 
+          to="tools"
+          title="Submit A Tool"
+          description="To submit a tool to the website please use our Tool Submission Form. This will create a new index in the Tool Index page, so that other developers can take advantage of this utility."
+        />
+
+				<HomePageItem
+          to="#"
+          title="Connect Now"
+          description="To learn more about the BLVSP community please visit our Community section. Here you can find other software professionals to discuss useful tools."
+        />
+
+        <HomePageItem
+          to="#"
+          title="Get Involved"
+          description="To get more engaged in the community please consider volunteering. Here you can help improve our Tool Index website through content moderation or community outreach."
+        />
 			</Grid>
 		</>
 	)
