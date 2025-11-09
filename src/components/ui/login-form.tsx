526128b5658561865353a0a993ea90e8ea79a5d8
elliotchun <eschun@uci.edu>
2025-11-08 21:20:40 -0800

    Implemented authentication through better-auth

--- src/components/ui/login-form.tsx
+++ src/components/ui/login-form.tsx
@@ -1,7 +1,28 @@
 import { Button, Field, Input } from "@chakra-ui/react";
 import { PasswordInput } from "./password-input";
+import { signIn, signUp } from "@/lib/auth";
+import { MouseEventHandler } from "react";
 
 export const LoginForm = () => {
+	const login: MouseEventHandler = (e) => {
+		e.preventDefault();
+		signIn.email({
+			email: "",
+			password: "",
+			callbackURL: "/"
+		});
+	}
+
+	const register: MouseEventHandler = (e) => {
+		e.preventDefault();
+		signUp.email({
+			name: "",
+			email: "",
+			password: "",
+			callbackURL: "/"
+		});
+	}
+
 	return (
 		<form className="flex flex-col">
 			<Field.Root>
@@ -12,7 +33,8 @@ export const LoginForm = () => {
 				<Field.Label>Password</Field.Label>
 				<PasswordInput />
 			</Field.Root>
-			<Button type="submit" mt="8" bg="primary">Login</Button>
+			<Button type="submit" mt="8" bg="primary" onClick={login}>Login</Button>
+			<Button type="submit" mt="8" bg="primary" onClick={register}>Register</Button>
 		</form>
 	)
 }
