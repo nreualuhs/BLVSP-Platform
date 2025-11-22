2cd1cba6862fde67712a1152430bd56265f1cf7f
elliotchun <eschun@uci.edu>
2025-11-21 20:10:54 -0800

    Create the tool submission form inputs

--- packages/client/src/pages/ToolSubmissionPage.tsx
+++ packages/client/src/pages/ToolSubmissionPage.tsx
@@ -1,21 +1,47 @@
-import { Heading, Link, Stack, Text } from "@chakra-ui/react";
+import { Button, Field, Heading, Input, Link, RadioGroup, Stack, Text, VStack } from "@chakra-ui/react";
+import { ReactNode } from "react";
+import { Form } from "react-router-dom";
 
 function ToolSubmissionPage() {
+  const FormInput = ({ label, required = true }: { label: ReactNode, required?: boolean }) => (
+    <Field.Root required={required}>
+      <Field.Label>
+        {label} <Field.RequiredIndicator />
+      </Field.Label>
+      <Input />
+    </Field.Root>
+  );
+
   return (
     <Stack gap={4}>
       <Heading size="4xl">
         Tool Submission Form
       </Heading>
-      <Text>
-        To submit a tool, please fill out the {' '}
-        <Link 
-          href="https://forms.gle/kjVtXty6fc8Wq95g8"
-          variant="underline"
-          colorPalette="blue"
-        >
-          Google Form here.
-        </Link>
-      </Text>
+      <Form>
+        <FormInput label="Email" />
+        <FormInput label="Link to Tool" />
+        <FormInput label="Description" />
+        <FormInput label="Compatability Information" required={false} />
+        <FormInput label="Tutorial Video(s)" required={false} />
+        <FormInput label="Creator's Guidelines/Cautions for the Tool" required={false} />
+        <FormInput label="Limitations" required={false} />
+        <FormInput label="Extra Comments" required={false} />
+        <RadioGroup.Root>
+          <VStack align="start">
+            <RadioGroup.Item value="Yes">
+              <RadioGroup.ItemHiddenInput></RadioGroup.ItemHiddenInput>
+              <RadioGroup.ItemIndicator></RadioGroup.ItemIndicator>
+              <RadioGroup.ItemText>Yes</RadioGroup.ItemText>
+            </RadioGroup.Item>
+            <RadioGroup.Item value="No">
+              <RadioGroup.ItemHiddenInput></RadioGroup.ItemHiddenInput>
+              <RadioGroup.ItemIndicator></RadioGroup.ItemIndicator>
+              <RadioGroup.ItemText>No</RadioGroup.ItemText>
+            </RadioGroup.Item>
+          </VStack>
+        </RadioGroup.Root>
+        <Button variant="outline">Submit</Button>
+      </Form>
     </Stack>
   );
 }
